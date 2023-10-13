fd3280a5258e51d13e4893a651427bbad1f4b31c
johnvak01 <johnvak01@gmail.com>
2023-10-12 18:56:07 -0600

    added bootstrap 5 to Django

--- VDevSite/settings.py
+++ VDevSite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'd1_counterapp'
+    'd1_counterapp',
+    'bootstrap5'
     ]
 
 MIDDLEWARE = [
