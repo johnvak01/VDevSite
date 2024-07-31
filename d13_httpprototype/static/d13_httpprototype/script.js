c8078631ff89d2e89539f3e7b2a41128265f29eb
johnvak01 <johnvak01@gmail.com>
2024-07-31 15:20:39 -0600

    added better button to perform the same thing but with the fetch api instead of ajax

--- d13_httpprototype/static/d13_httpprototype/script.js
+++ d13_httpprototype/static/d13_httpprototype/script.js
@@ -29,3 +29,59 @@ function getUsers(e){
   httpRq.send();
 }
 
+const buttonbtr = document.getElementById("btnbtr");
+buttonbtr.addEventListener("click", getUsersBetter);
+function getUsersBetter(e){
+  e.preventDefault();
+    
+  fetch("http://127.0.1:8000/d13_httpprototype/GetJSON")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+  
+    let output = "<h1>Users<h1>";
+    data.forEach(function(user){
+      output+=
+      `
+      <hr>
+      <h2> User ${user.id} - ${user.name}: Age ${user.age}</h2>
+      `
+    });
+    document.getElementById("usersbtr").innerHTML = output;
+  });
+
+
+
+
+
+/*
+
+  const httpRq = new XMLHttpRequest();
+  httpRq.open("GET", "http://127.0.0.1:8000/d13_httpprototype/GetJSON",true);
+  httpRq.onload = function(){
+    if(this.status === 200 ){
+      console.log(this.responseText);
+      const users = JSON.parse(this.responseText);
+      
+      let output = "<h1>Users<h1>";
+      
+      users.forEach(function(user){
+        output+=
+        `
+        <hr>
+        <h2> User ${user.id} - ${user.name}: Age ${user.age}</h2>
+        `
+      })
+    document.getElementById("usersbtr").innerHTML = output;
+
+
+    } else{
+      console.log(this.responseText);
+    }
+  }
+  httpRq.send();
+*/
+}
+
