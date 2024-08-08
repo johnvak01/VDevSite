731109f20f15a845fac9378407d41c99fb53b998
johnvak01 <johnvak01@gmail.com>
2024-08-08 12:17:20 -0600

    project finished

--- d22_activemenu/static/d22_activemenu/script.js
+++ d22_activemenu/static/d22_activemenu/script.js
@@ -1,7 +1,12 @@
 const links = document.querySelectorAll(".nav-list li a");
 for(link of links){
   link.addEventListener("click", smoothScroll);
+  
 }
+
+const nav_list = document.querySelector(".nav-list");
+nav_list.addEventListener("click",updateActive);
+
 
 function smoothScroll(e){
   e.preventDefault();
@@ -15,3 +20,13 @@ window.addEventListener("scroll",()=>{
   header.classList.toggle("sticky",window.scrollY > 0);
   
 });
+
+function updateActive(e){
+  const nav_link = e.target.parentElement;
+  if(nav_link.classList.contains("link")){
+    nav_list.querySelector(".active").classList.remove("active");
+    nav_link.classList.add("active");
+  }
+  
+
+}
