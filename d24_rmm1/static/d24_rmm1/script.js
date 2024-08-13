045345a8d66d7db376213f4e32261ce8135c6145
johnvak01 <johnvak01@gmail.com>
2024-08-13 12:57:07 -0600

    updated css and javascript

--- d24_rmm1/static/d24_rmm1/script.js
+++ d24_rmm1/static/d24_rmm1/script.js
@@ -1,52 +1,37 @@
 const links = document.querySelectorAll(".nav-list li a");
 for(link of links){
   link.addEventListener("click", smoothScroll);
-  
 }
-
-const nav_list = document.querySelector(".nav-list");
-nav_list.addEventListener("click",updateActive);
 
 function smoothScroll(e){
   e.preventDefault();
-
+  
   const href = this.getAttribute("href");
   document.querySelector(href).scrollIntoView({behavior: "smooth"});
-}
-
-window.addEventListener("scroll",()=>{
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky",window.scrollY > 0);
   
-});
-
-function updateActive(e){
-  const nav_link = e.target.parentElement;
-  if(nav_link.classList.contains("link")){
-    nav_list.querySelector(".active").classList.remove("active");
-    nav_link.classList.add("active");
+  console.log(window.innerWidth);
+  if(window.innerWidth < 600){
+  hideMenu();
   }
+
 }
 
-const scroll_btn = document.querySelector(".top");
-const root_el = document.documentElement;
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".bi-list");
+const close = document.querySelector(".bi-x");
 
-document.addEventListener("scroll", showButton);
-scroll_btn.addEventListener("click", ScrollToTop);
-
-function showButton(){
-  const scrollTotal=root_el.scrollHeight - root_el.clientHeight;
-  if(root_el.scrollTop/scrollTotal > 0.3){
-    scroll_btn.classList.add("show_button");
-  }else{
-    scroll_btn.classList.remove("show_button");
-  }
+const showMenu = ()=>{
+  hamburger.style.display="none";
+  close.style.transform="translateY(-5px)";
+  menu.style.transform="TranslateY(0)";
 }
 
-function ScrollToTop(e){
-e.preventDefault();
-  root_el.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+const hideMenu = ()=>{
+  hamburger.style.display="block";
+  close.style.transform="translateY(-20rem)";
+  menu.style.transform="TranslateY(-200rem)";
 }
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
+window.addEventListener("resize",()=>{if(window.innerWidth>600){hamburger.style.display="none";close.style.transform="transformY(-200rem)";}else{hamburger.style.display="block";}});
