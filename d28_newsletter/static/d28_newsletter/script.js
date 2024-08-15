1126cc51d9c8becdb70dec80352a94e8d4d0e89b
johnvak01 <johnvak01@gmail.com>
2024-08-15 15:21:03 -0600

    added JS for the newsletter to slide in and out. no funcitonality for submitting yet

--- d28_newsletter/static/d28_newsletter/script.js
+++ d28_newsletter/static/d28_newsletter/script.js
@@ -10,15 +10,18 @@ function smoothScroll(e){
   document.querySelector(href).scrollIntoView({behavior: "smooth"});
 }
 
+let nlinteract = false;
+
 window.addEventListener("scroll",()=>{
   const header = document.querySelector("header");
   const progressBar = document.querySelector(".progress");
   const scrollIndicator = document.querySelector(".scroll-indicator");
   header.classList.toggle("sticky",window.scrollY > 0);
+  const current_state= document.body.scrollTop || document.documentElement.scrollTop;
+  const page_height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scroll_percent = (current_state/page_height)*100;
+
   if(window.scrollY > 0){
-    const current_state= document.body.scrollTop || document.documentElement.scrollTop;
-    const page_height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scroll_percent = (current_state/page_height)*100;
 
     progressBar.style.visibility = "visible";
     scrollIndicator.style.visibility = "visible";
@@ -27,7 +30,19 @@ window.addEventListener("scroll",()=>{
     progressBar.style.visibility = "hidden";
     scrollIndicator.style.visibility = "hidden";
   } 
+  
+  if(scroll_percent>=75 && nlinteract === false){
+    const newsletter = document.querySelector(".newsletter");
+    newsletter.style.transform = "TranslateX(0)";
+  }
+});
 
+const newsclose = document.querySelector(".bi-x");
+newsclose.addEventListener("click",()=>{
+    const newsletter = document.querySelector(".newsletter");
+    newsletter.style.transform = "TranslateX(-100%)";
+  nlinteract = true;
+  console.log("closed");
 });
 
 
